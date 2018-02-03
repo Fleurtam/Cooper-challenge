67f6613430814381642ab338f2f9419bb2f78920
FleurTamboezer <ftamboezer@gmail.com>
2018-02-03 14:23:38 +0100

    Started on male results cooper test

---
+++ src/cooper.js
@@ -0,0 +1,128 @@
+function CooperResult() {}
+
+CooperResult.prototype.male_result = function(obj) {
+
+switch(age) {
+  case if (age = 13||14) { :
+     for (distance)
+        if (distance < 2100) {
+           result = 'Poor';
+        } else if (distance >= 2100 && distance<= 2199) {
+           result = 'Below Average';
+           break;
+        } else if (distance >= 2200 && distance<= 2399) {
+           result = 'Average';
+           break;
+        } else if (distance >= 2400 && distance<= 2699) {
+           result = 'Above Average';
+           break;
+        } else if (distance >2700) {
+           result = 'Excellent';
+              break;
+
+
+
+        case if (age = 15||16) { :
+                 for (distance)
+                    if (distance < 2200) {
+                       result = 'Poor';
+                    } else if (distance >= 2200 && distance<= 2299) {
+                       result = 'Below Average';
+                       break;
+                    } else if (distance >= 2300 && distance<= 2499) {
+                       result = 'Average';
+                       break;
+                    } else if (distance >= 2500 && distance<= 2799) {
+                       result = 'Above Average';
+                       break;
+                    } else if (distance >2800) {
+                       result = 'Excellent';
+                          break;
+
+                          case if (age >= 17 && age <= 19) { :
+                                   for (distance)
+                                      if (distance < 2300) {
+                                         result = 'Poor';
+                                      } else if (distance >= 2300 && distance<= 2499) {
+                                         result = 'Below Average';
+                                         break;
+                                      } else if (distance >= 2500 && distance<= 2699) {
+                                         result = 'Average';
+                                         break;
+                                      } else if (distance >= 2700 && distance<= 2999) {
+                                         result = 'Above Average';
+                                         break;
+                                      } else if (distance >3000) {
+                                         result = 'Excellent';
+                                            break;
+
+                                            case if (age >= 20 && age <= 29) { :
+                                                     for (distance)
+                                                        if (distance < 1600) {
+                                                           result = 'Poor';
+                                                        } else if (distance >= 1600 && distance<= 2199) {
+                                                           result = 'Below Average';
+                                                           break;
+                                                        } else if (distance >= 2200 && distance<= 2399) {
+                                                           result = 'Average';
+                                                           break;
+                                                        } else if (distance >= 2400 && distance<= 2799) {
+                                                           result = 'Above Average';
+                                                           break;
+                                                        } else if (distance >2800) {
+                                                           result = 'Excellent';
+                                                              break;
+
+                                                                case if (age >= 30 && age <= 39) { :
+                                                                       for (distance)
+                                                                          if (distance < 1500) {
+                                                                             result = 'Poor';
+                                                                          } else if (distance >= 1500 && distance<= 2299) {
+                                                                             result = 'Below Average';
+                                                                             break;
+                                                                          } else if (distance >= 2000 && distance<= 2299) {
+                                                                             result = 'Average';
+                                                                             break;
+                                                                          } else if (distance >= 2300 && distance<= 2699) {
+                                                                             result = 'Above Average';
+                                                                             break;
+                                                                          } else if (distance >2700) {
+                                                                             result = 'Excellent';
+                                                                                break
+
+                                                                                case if (age >= 40 && age <= 49) { :
+                                                                                       for (distance)
+                                                                                          if (distance < 1400) {
+                                                                                             result = 'Poor';
+                                                                                          } else if (distance >= 1400 && distance<= 1699) {
+                                                                                             result = 'Below Average';
+                                                                                             break;
+                                                                                          } else if (distance >= 1700 && distance<= 2099) {
+                                                                                             result = 'Average';
+                                                                                             break;
+                                                                                          } else if (distance >= 2100 && distance<= 2499) {
+                                                                                             result = 'Above Average';
+                                                                                             break;
+                                                                                          } else if (distance >2500) {
+                                                                                             result = 'Excellent';
+                                                                                                break
+
+
+                                                                                                case if (age >= 50) { :
+                                                                                                       for (distance)
+                                                                                                          if (distance < 130) {
+                                                                                                             result = 'Poor';
+                                                                                                          } else if (distance >= 1300 && distance<= 1599) {
+                                                                                                             result = 'Below Average';
+                                                                                                             break;
+                                                                                                          } else if (distance >= 1600 && distance<= 1999) {
+                                                                                                             result = 'Average';
+                                                                                                             break;
+                                                                                                          } else if (distance >= 2000 && distance<= 2399) {
+                                                                                                             result = 'Above Average';
+                                                                                                             break;
+                                                                                                          } else if (distance >2400) {
+                                                                                                             result = 'Excellent';
+                                                                                                                break
+
+}
